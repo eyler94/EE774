a1c7746bf1618c69b3833343b8d75bc25205cfcf
Michael Eyler <eyler94@gmail.com>
2020-02-13 10:34:07 -0700

    Switching to desktop.

--- hw3/hw3p2.m
+++ hw3/hw3p2.m
@@ -10,3 +10,51 @@ eq3 = 0 == w1*w2;
 
 solve([eq1, eq2, eq3])
 
+
+%% part C
+
+syms lmbda_s I1 I2 I3 lmbda
+
+A1 = [0 0 0;
+      0 0 lmbda_s*(I3-1)/I2;
+      0 lmbda_s*(1-I2)/I3 0];
+  
+eq1 = 0 == det(lmbda*eye(3)-A1);
+
+roots_1 = solve(eq1, lmbda)
+
+A2 = [0 0 lmbda_s*(1-I3)/I1;
+      0 0 0;
+      lmbda_s*(I1-1)/I3 0 0];
+  
+eq2 = 0 == det(lmbda*eye(3)-A2);
+
+roots_2 = solve(eq2, lmbda)
+
+A3 = [0 lmbda_s*(I2-1)/I1 0;
+      lmbda_s*(1-I1)/I2 0 0;
+      0 0 0];
+  
+eq3 = 0 == det(lmbda*eye(3)-A3);
+
+roots_3 = solve(eq3, lmbda)
+
+%% part D
+func_w = @(t,w) [w(2)*w(3)/3; (-2)*w(1)*w(3)/2; w(1)*w(2)]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
