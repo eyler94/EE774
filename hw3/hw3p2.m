e346025435319d3e6aa0fee5a308c2e8585c0b51
Michael Eyler <eyler94@gmail.com>
2020-02-13 17:48:40 -0700

    Finished most of it. Headed home to finish the rest.

--- hw3/hw3p2.m
+++ hw3/hw3p2.m
@@ -1,48 +1,20 @@
 clc
 clear
 close all
+%%%part D
+func_w = @(t,w) [w(2)*w(3)/3; (-2)*w(1)*w(3)/2; w(1)*w(2)];
 
-syms w1 w2 w3
+t_final = 10;
 
-eq1 = 0 == w2*w3;
-eq2 = 0 == w1*w3;
-eq3 = 0 == w1*w2;
-
-solve([eq1, eq2, eq3])
-
-
-%% part C
-
-syms lmbda_s I1 I2 I3 lmbda
-
-A1 = [0 0 0;
-      0 0 lmbda_s*(I3-1)/I2;
-      0 lmbda_s*(1-I2)/I3 0];
-  
-eq1 = 0 == det(lmbda*eye(3)-A1);
-
-roots_1 = solve(eq1, lmbda)
-
-A2 = [0 0 lmbda_s*(1-I3)/I1;
-      0 0 0;
-      lmbda_s*(I1-1)/I3 0 0];
-  
-eq2 = 0 == det(lmbda*eye(3)-A2);
-
-roots_2 = solve(eq2, lmbda)
-
-A3 = [0 lmbda_s*(I2-1)/I1 0;
-      lmbda_s*(1-I1)/I2 0 0;
-      0 0 0];
-  
-eq3 = 0 == det(lmbda*eye(3)-A3);
-
-roots_3 = solve(eq3, lmbda)
-
-%% part D
-func_w = @(t,w) [w(2)*w(3)/3; (-2)*w(1)*w(3)/2; w(1)*w(2)]
-
-
+hold on
+for iter = 0:1000
+    w=randi([-100,100],3,1);
+    w=w./norm(w);
+    [~, points] = ode45(func_w,[0,t_final],w);
+    plot3(points(:,1), points(:,2), points(:,3), 'LineWidth',1.5)
+    plot3(points(1,1), points(1,2), points(1,3), 'bo')
+    plot3(points(end,1), points(end,2), points(end,3), 'bs')
+end
 
 
 
--- hw3/hw3p8ab.m
+++ hw3/hw3p8ab.m
@@ -10,19 +10,32 @@ eq3 = 0 == x3^2*(1-x3);
 
 sol =  solve([eq1, eq2, eq3], [x1, x2, x3]);
 
-x1 = sol.x1
-x2 = sol.x2
-x3 = sol.x3
+x1_res = sol.x1
+x2_res = sol.x2
+x3_res = sol.x3
 
 %%% Part b
 
 syms lmbd
 
-A = [lmbd 1 1;
+res = [lmbd 1 1;
      -1 lmbd+1 -1;
      0 0 lmbd+1];
  
-eq4 = 0 == det(A);
+eq4 = 0 == det(res);
+sol = solve(eq4, lmbd)
 
-solve(eq4, lmbd)
-    
+A = [0 -1 -1;
+     1 -1 1;
+     0 0 -1];
+
+syms t
+eA = expm(A*t);
+eAt = expm(A'*t);
+P = int(eAt*eA,t,0,inf);
+
+X = [x1; x2; x3];
+
+V = X'*P*X
+
+
